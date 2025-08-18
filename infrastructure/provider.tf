95bc352bd7a61b91f73c463e03e8685fc74aa161
彻 <72044077+xuelinxuan@users.noreply.github.com>
2025-08-18 18:00:35 +0200

    Update provider.tf

--- infrastructure/provider.tf
+++ infrastructure/provider.tf
@@ -2,12 +2,11 @@ provider "aws" {
   region = var.aws_region
 }
 
-# 未来文件的内容都写在key这个路径下，这里都是可以自定义的
-# Centralizar o arquivo de controle de estado do terraform
-terraform {
-  backend "s3" {
-    bucket = "terraform-state-igti-ney"
-    key    = "state/igti/edc/mod1/terraform.tfstate"
-    region = "us-east-2"
-  }
-}
+# 如需远程状态（S3 backend），请手动创建 bucket 并取消注释以下内容
+# terraform {
+#   backend "s3" {
+#     bucket = "terraform-state-igti-ney"
+#     key    = "state/igti/edc/mod1/terraform.tfstate"
+#     region = "us-east-2"
+#   }
+# }
